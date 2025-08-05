ce390010dbd5bea06d431b8b5c5f9ed733a7b203
Chetan Sarswat <sarswatchetan97@gmail.com>
2025-08-05 22:41:55 +0530

    Created frame of header and main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
 import React from "react"
+import "./App.css"
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello Chetan</h1>
+    <div className="app">
+      <header className="app_header">Jira Board</header>
+      <main className="app_main">
+        <section className="task_column">Section1</section>
+        <section className="task_column">Section1</section>
+        <section className="task_column">Section1</section>
+        <section className="task_column">Section1</section>
+      </main>
     </div>
   )
 };
