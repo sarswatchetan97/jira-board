68baa3405df0da89da28b5fcda4f5c95ba13661d
Chetan Sarswat <sarswatchetan97@gmail.com>
2025-08-09 22:54:00 +0530

    Add CSS to input field and buttons

--- src/components/TaskForm/TaskForm.jsx
+++ src/components/TaskForm/TaskForm.jsx
@@ -1,25 +1,38 @@
-import React from "react"
+import React from "react";
+import "./TaskForm.css";
 
 const TaskForm = () => {
-    return (
-        <header className="app_header">
-            <form>
-                <input type="text" className="task_input" placeholder="Enter Task Detail" />
-            </form>
-            <div className="">
-                <button className="tag">DEV</button>
-                <button className="tag">QA</button>
-                <button className="tag">Product Owner</button>
-            </div>
+  return (
+    <header className="app_header">
+      <form>
+        <input
+          type="text"
+          className="task_input"
+          placeholder="Enter Task Detail"
+        />
+        <div className="task_form_bottom">
+          <div>
+            <button className="tag">DEV</button>
+            <button className="tag">QA</button>
+            <button className="tag">Product Owner</button>
+          </div>
+          <div>
             <select className="task_status">
-                <option value='Ready for Development'>Ready for Development</option>
-                <option value='In Progress'>In Progress</option>
-                <option value='Ready for Test'>Ready for Test</option>
-                <option value='Closed'>Closed✅</option>
+              <option value="Ready for Development">
+                Ready for Development
+              </option>
+              <option value="In Progress">In Progress</option>
+              <option value="Ready for Test">Ready for Test</option>
+              <option value="Closed">Closed✅</option>
             </select>
-            <button type="submit" className="task_submit">+ Add</button>
-        </header>
-    )
+            <button type="submit" className="task_submit">
+              + Add
+            </button>
+          </div>
+        </div>
+      </form>
+    </header>
+  );
 };
 
 export default TaskForm;
